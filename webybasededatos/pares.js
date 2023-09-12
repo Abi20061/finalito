90f75c574d181546bb087e97e21c2e872442c1af
Abi20061 <124410675+Abi20061@users.noreply.github.com>
2023-09-12 09:53:21 -0700

    Add files via upload
    12/09/2023 necesita ajustes

---
+++ webybasededatos/pares.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    var carta1=""; var carta2="";
+    var par =false;
+    var total_pares=0;
+    $('img').click(function(e){
+        var estado=$(this).attr('data-estado')
+        var nombre_imagen=$(this).attr('data-id')
+
+        if(estado=="0"){
+            if (carta1 !="" && carta2 !=""){
+                console.log("las cartas son diferentes")
+                if(carta1.attr('data-id') !=carta2.attr('data-id')){
+                    console.log("Tapa carta diferentes")
+                    carta1.attr('src', 'imagenes/principal' + nombre_imagen)
+                    carta2.attr('src', 'imagenes/principal' + nombre_imagen)
+                }else{
+                    console.log("las cartas son iguales")
+                    carta1.attr('data-estado', '1')
+                    carta2.attr('data-estado', '1')
+                    par=false
+                    
+                }
+                carta1=""
+                carta2=""
+            }
+            console.log("Estado de la carta:" + estado)
+            if (carta1==""){
+                carta1=$(this);
+                carta1.attr('src', 'imagenes/principal' + nombre_imagen)
+                console.log("se asigno carta #1")
+            }else{
+                carta2=$(this);
+                carta2.attr('src', 'imagenes/principal' + nombre_imagen)
+                console.log("se asigno carta #2")
+            }
+        }
+        
+        if(carta1 !="" && carta2 !=""){
+            if(carta1.attr('data-id')==carta2.attr('data-id')){
+             par=true
+             total_pares++
+             $("#total_pares").html(total_pares)
+
+ 
+            } 
+        }
+        if(total_pares==8){
+            $('img').each(function(){
+                $(this).attr('src', 'imagenes/principal' + nombre_imagen)
+                $(this).attr('data-estado', '0')
+            });
+            total_pares=0
+            par=false
+            carta1=""
+            carta2=""
+            return;
+        }
+        
+    });
+});
